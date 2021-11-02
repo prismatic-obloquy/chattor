1aa85cca6e12e4874d8548de0e940bc7a29563e9
Prismatic Obloquy <prismatic@obloquy.work>
2021-11-01 22:52:21 -0400

    fix intentionally broken thing

--- src/test/add1.c
+++ src/test/add1.c
@@ -7,13 +7,7 @@ void test_1plus1_eq_2(void) {
     CU_ASSERT_EQUAL(output, 2);
 }
 
-void test_failure(void) {
-    int output = add1(3);
-    CU_ASSERT_EQUAL(output, 2);
-}
-
 TESTS {
     SUITE(add1, NULL, NULL);
     TEST(add1, test_1plus1_eq_2);
-    TEST(add1, test_failure);
 }
